27683eae9380d132a57042dbc5c97f9bc6cbb085
mikajoos-cmyk <mika.joos@joos-soft-solutions.de>
2025-10-06 19:58:49 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -37,7 +37,7 @@ interface DocumentFile {
 
 // --- HILFSFUNKTIONEN ---
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
-
+console.log(API_BASE_URL);
 // Neuer API-Helfer
 const apiClient = {
   get: async (path: string, token: string | null) => {
