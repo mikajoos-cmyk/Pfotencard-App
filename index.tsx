7a33d4cbd31d363b7233185727d9ad80569476aa
mikajoos-cmyk <mika.joos@joos-soft-solutions.de>
2025-10-06 18:34:44 +0200

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -37,7 +37,7 @@ interface DocumentFile {
 
 // --- HILFSFUNKTIONEN ---
 // --- HILFSFUNKTIONEN ---
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
+const API_BASE_URL = 'https://pfotencard-backend.onrender.com' || 'http://127.0.0.1:8000';
 
 // Neuer API-Helfer
 const apiClient = {
